ded11502cff9093b65d7b6590a3c653b7227ef01
ryverine <rwcheek@gmail.com>
2019-04-13 18:52:24 -0400

    Making sure latest code in in rep0.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,123 +1,68 @@
-// https://www.stardewvalley.net/
-// https://stardewvalleywiki.com/Stardew_Valley_Wiki
-// https://soundcloud.com/concernedape/spring-the-valley-comes-alive
-// https://www.reddit.com/r/StardewValley/comments/b2mvop/stardew_valley_phone_wallpaper/
-// https://www.reddit.com/r/StardewValley/comments/7e67r7/looking_for_a_specific_wallpaper/
-// https://stardewvalleywiki.com/List_of_All_Gifts
-// https://stardewvalleywiki.com/Calendar
-
-// https://www.reddit.com/r/StardewValley/comments/4dtgp7/by_popular_request_a_stardew_valley_font_for_your/
-// https://www.dropbox.com/sh/g1law0qmnf6pjwr/AACummg5fZJ5JIF4ReeRDxJia?dl=0
-
-
-// https://stardewvalley.fandom.com/wiki/Villager_marriage_candidates
-
-
-
-/**
- * DO IMAGES LIKE THIS
- * 		var catImage = $("<img>");
- * 		catImage.attr("src", imageUrl);
- * 		catImage.attr("alt", "cat image");
- * 		$("#images").prepend(catImage);
- */
 
 $(document).ready(function() 
 {
 	var questions = [];
-
 	var currentQuestion = 0;
-
 	var questionDiv = $("#questionDiv");
-
 	var answerDiv = $("#answerDiv");
-
 	var resultsDiv = $("#resultsDiv");
-
 	var timerDiv = $("#timerDiv");
-
 	var secondCountSpan = $("#secondCount");
-
 	var restartButton = $("#btn-restart");
-
 	var intervalId;
-
 	var timerRunning = false;
-
 	var secondCounter = 10;
 
 
 	$("#btn-start").on("click", function() 
 	{	
 		$("#btn-start").hide();
-
 		questions = loadQuestions();
-
 		getQuestion();
-
 	});
 
 
 	$("#btn-answerA").on("click", function() 
 	{	
 		console.log(this.value);
-
 		stopTimer();
-
 		logAnswer(this.value);
-  		
 	});
 
 
 	$("#btn-answerB").on("click", function() 
 	{	
 		console.log(this.value);
-
 		stopTimer();
-
 		logAnswer(this.value);
-  		
 	});
 
 
 	$("#btn-answerC").on("click", function() 
 	{	
 		console.log(this.value);
-
 		stopTimer();
-
 		logAnswer(this.value);
-  		
 	});
 
 
 	$("#btn-answerD").on("click", function() 
 	{	
 		console.log(this.value);
-
 		stopTimer();
-
 		logAnswer(this.value);
-  		
 	});
 
 
 	$("#btn-restart").on("click", function() 
 	{	
 		console.log(this.value);
-
 		restartButton.hide();
-
 		questions = [];
-
 		currentQuestion = 0;
-
 		timerRunning = false;
-
 		secondCounter = 10;
-
 		questions = loadQuestions();
-
 		getQuestion();
 	});
 
@@ -125,13 +70,9 @@ $(document).ready(function()
 	function stopTimer() 
 	{
 		clearInterval(intervalId);
-
 		timerRunning = false;
-
 		secondCounter = 10;
-
 		secondCountSpan.text("10");
-
 		timerDiv.hide();
 	}
 
@@ -141,9 +82,7 @@ $(document).ready(function()
   		if (!timerRunning)
   		{	
   			timerDiv.show();
-
     		intervalId = setInterval(countTime,1000);
-
     		clockRunning = true;
     	}
   	}
@@ -152,32 +91,22 @@ $(document).ready(function()
   	function countTime() 
   	{
   		secondCounter--;
-
   		secondCountSpan.text(secondCounter);
 
   		if (secondCounter === 0)
   		{	
 			stopTimer();
-
 			answerDiv.hide();
-
 			resultsDiv.show();
-
 			resultsDiv.html("<div class='resultHeader'>" + "<img src='" + "assets/images/timesUp.png" + "'>" + "</div>");
-
 			var tmpDiv = $("<div>")
 			tmpDiv.attr("class", "responseText");
-
 			resultsDiv.append(	"<div>" + "<h3>" + "The correct answer is: " + "</h3>" + "</div>" +
-								"<div>" + "<h1>" + questions[currentQuestion].answers[questions[currentQuestion].correctAnswer] + "</h1>" + "</div");
-							
+								"<div>" + "<h1>" + questions[currentQuestion].answers[questions[currentQuestion].correctAnswer] + "</h1>" + "</div");			
 			tmpDiv.append("<div id='img-fluid questionImg'>" + "<img src='" + questions[currentQuestion].imgUrl + "'>" + "</div>");
-
 			resultsDiv.append(tmpDiv);
-
 			currentQuestion++;
-
-			setTimeout(getQuestion, 5000);
+			setTimeout(getQuestion, 6000);
   		}
   	}
 
@@ -185,33 +114,23 @@ $(document).ready(function()
 	function getQuestion()
 	{
 		resultsDiv.text("");
-
 		secondCountSpan.text("10");
-
 		timerDiv.show();
-
 		answerDiv.show();
 
 		if(currentQuestion < questions.length)
 		{
 			questionDiv.text(questions[currentQuestion].question);
-
 			questionDiv.show();
-
 			$("#btn-answerA").text(questions[currentQuestion].answers[0]);
-
 			$("#btn-answerB").text(questions[currentQuestion].answers[1]);
-
 			$("#btn-answerC").text(questions[currentQuestion].answers[2]);
-
 			$("#btn-answerD").text(questions[currentQuestion].answers[3]);
-
 			startTimer();
 		}
 		else
 		{
 			console.log("HAVE ANSWERED ALL QUESTIONS");
-
 			gradeQuiz();
 		}
 	}
@@ -220,7 +139,6 @@ $(document).ready(function()
 	function logAnswer(theAnswer)
 	{
 		answerDiv.hide();
-
 		resultsDiv.show();
 
 		switch(theAnswer.toUpperCase())
@@ -228,19 +146,15 @@ $(document).ready(function()
   			case "A":
     			questions[currentQuestion].selectedAnswer = 0;
    				break;
-
   			case "B":
     			questions[currentQuestion].selectedAnswer = 1;
     			break;
-
   			case "C":
     			questions[currentQuestion].selectedAnswer = 2;
     			break;
-
   			case "D":
     			questions[currentQuestion].selectedAnswer = 3;
     			break;
-
   			default:
     			console.log("logAnswer(" + theAnswer + "): Unexpected argument.");
 		}
@@ -248,35 +162,25 @@ $(document).ready(function()
 		if(questions[currentQuestion].selectedAnswer === questions[currentQuestion].correctAnswer)
 		{
 			resultsDiv.html("<div class='resultHeader'>" + "<img src='" + "assets/images/youGotIt.png" + "'>" + "</div>");
-
 			var tmpDiv = $("<div>")
 			tmpDiv.attr("class", "responseText");
-
 			tmpDiv.append("<div id='img-fluid questionImg'>" + "<img src='" + questions[currentQuestion].imgUrl + "'>" + "</div>");
-
 			resultsDiv.append(tmpDiv);
-
 			currentQuestion++;
-
-			setTimeout(getQuestion, 5000);
+			setTimeout(getQuestion, 6000);
 		}
 		else
 		{
 			resultsDiv.html("<div class='resultHeader'>" + "<img src='" + "assets/images/youDidntGetIt.png" + "'>" + "</div>");
-
 			var tmpDiv = $("<div>")
 			tmpDiv.attr("class", "responseText");
-
 			tmpDiv.html(	"<div>" + "<h3>" + "The correct answer is: " + "<h3>" + "</div>" +
 								"<div>" + "<h1>" + questions[currentQuestion].answers[questions[currentQuestion].correctAnswer] + "</h1>" + "</div");
 
 			tmpDiv.append("<div id='img-fluid questionImg'>" + "<img src='" + questions[currentQuestion].imgUrl + "'>" + "</div>");
-
 			resultsDiv.append(tmpDiv);
-
 			currentQuestion++;
-
-			setTimeout(getQuestion, 5000);
+			setTimeout(getQuestion, 6000);
 		}
 	}
 
@@ -284,13 +188,9 @@ $(document).ready(function()
 	function gradeQuiz()
 	{
 		timerDiv.hide();
-
 		questionDiv.hide();
-
 		answerDiv.hide();
-
 		resultsDiv.show();
-
 		var numRight = 0;
 		var numWrong = 0;
 		var total = questions.length;
@@ -329,7 +229,6 @@ $(document).ready(function()
 								"Total Questions: " + total + "<br>" + 
 								"Correct Answers: " + numRight + "<br>" + 
 								"Wrong Answers: " + numWrong + "</div>" );
-
 		restartButton.show();
 	}
 
@@ -337,9 +236,7 @@ $(document).ready(function()
 	function loadQuestions()
 	{
 		var questionList = getQuestions();
-
 		var finalQuestionList = [];
-
 		var addToFinalQuestionList = false;
 
 		while(finalQuestionList.length < 10)
